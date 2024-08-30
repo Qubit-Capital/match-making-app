2e6acca47ad16e60f33c10d468cfb2dbb1e2274c
Kshitiz Agrawal <kagrawal29@gmail.com>
2024-08-31 01:28:29 +0530

    Update index.tsx
    Removed unrequired test line

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Match Making App. This is a test line.</h1>
+      <h1 className="text-4xl font-bold mb-8">Welcome to Match Making App</h1>
       <div className="flex gap-4">
         <Link
           href="/register"
